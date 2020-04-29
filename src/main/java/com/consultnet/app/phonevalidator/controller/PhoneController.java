df71adbbff39fd4fd740b0a37fbaf29950a1aa68
johnpaulmariano <john.mariano.ctr@usuhs.edu>
2020-04-28 22:39:53 -0400

    Allow Access Origin

--- src/main/java/com/consultnet/app/phonevalidator/controller/PhoneController.java
+++ src/main/java/com/consultnet/app/phonevalidator/controller/PhoneController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,13 +24,14 @@ import com.consultnet.app.phonevalidator.model.Phone;
 import com.consultnet.app.phonevalidator.service.PhoneService;
 import com.consultnet.app.phonevalidator.service.PhoneValidatorService;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/phone")
 public class PhoneController {
 	
 	@Autowired
 	private PhoneService phoneService;
+	
 	
 	@Autowired
 	private PhoneValidatorService phoneValidatorService;
@@ -39,6 +41,7 @@ public class PhoneController {
 		return "working";
     }
 	
+	@CrossOrigin(origins = "http://localhost:3000")
 	@PostMapping("/add")
 	public @ResponseBody Map<String,Boolean> addNewPhone(@Valid @RequestBody Phone obj) {			
 		if(phoneValidatorService.validatePhoneNumber(obj.getNumber())) {
